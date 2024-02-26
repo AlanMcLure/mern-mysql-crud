e7981c925d2fa770198cd5923654584c9c027ac8
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-02-26 23:41:23 +0100

    crud hecho

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,31 +1,55 @@
 import { pool } from "../db.js";
 
 export const getTasks = async (req, res) => {
-  const [rows] = await req.context.models.Task.findAll();
-  return res.send(rows);
+  try {
+    const [result] = await pool.query('SELECT * FROM tasks ORDER BY createAt ASC');
+    return res.send(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  
 }
 
 export const getTask = async (req, res) => {
-  const [rows] = await req.context.models.Task.findAll();
-  return res.send(rows);
+  try {
+    const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', [req.params.id]);
+    
+    if (result.length === 0) return res.status(404).json({ message: 'Tarea no encontrada' });
+
+    return res.send(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 export const createTask = async (req, res) => {
   try {
     const { title, description } = req.body;
     const [result] = await pool.query('INSERT INTO tasks (title, description) VALUES (?, ?)', [ title, description ]);
-    res.json({id: result.insertId, title, description});
+    return res.json({id: result.insertId, title, description});
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 }
 
 export const updateTask = async (req, res) => {
-  const [rows] = await req.context.models.Task.findAll();
-  return res.send(rows);
-}
+  try {
+    const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    return res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const deleteTask = async (req, res) => {
-  const [rows] = await req.context.models.Task.findAll();
-  return res.send(rows);
+  try {
+    const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', [req.params.id]);
+    if (result.affectedRows === 0) return res.status(404).json({ message: 'Tarea no encontrada' });
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
