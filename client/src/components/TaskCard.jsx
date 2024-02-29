fa623d62d62294d5f762079369561d70a0cce025
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-03-01 00:30:25 +0100

    delete tasks

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
+import { deleteTaskRequest } from "../api/tasks.api";
+
 function TaskCard({ task }) {
+  
+  const handleDelete = async (id) => {
+    try {
+      console.log("Deleting task with id: ", id);
+      await deleteTaskRequest(id);
+      alert("Task deleted successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   return (
     <div>
       <h2>{task.title}</h2>
       <p>{task.description}</p>
       <span>{task.done == 1 ? "Sí" : "No"}</span>
       <button>Editar</button>
-      <button>Eliminar</button>
+      <button onClick={() => handleDelete(task.id)}>Eliminar</button>
     </div>
   )
 }
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -14,15 +14,20 @@ function TasksPage() {
     loadTasks();
   }, []);
 
+  function renderMain() {
+    
+    if (tasks.length === 0) return <p>No tasks found</p>;
+    
+    return tasks.map(task => (
+      <TaskCard task={task} key={task.id} />
+    ));
+  }
+
   return (
     <div>
       <h1>Tasks</h1>
 
-      <ul>
-        {tasks.map(task => (
-          <TaskCard task={task} key={task.id} />
-        ))}
-      </ul>
+        {renderMain()}
     </div>
   )
 }
